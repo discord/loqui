f96595ac5c57ef22226d4a9296e35f2bfa05c9f3
Jake <me@jh.gg>
2016-11-22 00:54:43 -0800

    cleanup redundant code in examples.

--- py/examples/client_bench_2.py
+++ py/examples/client_bench_2.py
@@ -1,81 +1,8 @@
-import time
-from greenlet import GreenletExit
-from traceback import print_exc
-
+from client_bench_base import run_client_bench
 from gevent.monkey import patch_all
 patch_all()
 
 from drpc.client import DRPCClient
 
-
-i = 0
-in_flight = set()
-failed_requests = 0
-
 client = DRPCClient(('localhost', 4001))
-
-
-class Request(object):
-    def __init__(self):
-        self.start_time = time.time()
-
-
-def do_work():
-    global i, last, failed_requests
-    r = Request()
-    in_flight.add(r)
-
-    try:
-        res = client.send_request('hello world')
-        # gevent.sleep(0)
-        # print repr(res)
-        assert res == 'hello world'
-        i += 1
-    except GreenletExit:
-        raise
-
-    except:
-        failed_requests += 1
-        print_exc()
-
-    finally:
-        in_flight.remove(r)
-
-
-def log_loop():
-    last_i = 0
-    last = time.time()
-    while True:
-        gevent.sleep(1)
-        now = time.time()
-        elapsed = now - last
-        req_sec = (i - last_i) / elapsed
-
-        max_age = max(now - r.start_time for r in in_flight) if in_flight else 0
-        print '%s total requests (%.2f/sec). last log %.2f sec ago. %s failed, %s in flight, %.2f ms max in flight age' % (
-            i, req_sec, elapsed, failed_requests, len(in_flight), max_age * 1000
-        )
-        last_i = i
-        last = now
-
-
-def work_loop():
-    print 'spawning work loop'
-    global i, last
-
-    while True:
-        do_work()
-
-
-import gevent
-client.send_request('hi')
-greenlets = [gevent.spawn(log_loop)]
-for _ in xrange(100):
-    greenlets.append(gevent.spawn(work_loop))
-
-import signal
-
-signal.signal(signal.SIGINT, lambda *_: gevent.killall(greenlets, block=False))
-
-gevent.joinall(greenlets)
-print 'All greenlets have exited.'
+run_client_bench(client, concurrency=100)
---
+++ py/examples/client_bench_base.py
@@ -0,0 +1,76 @@
+import time
+from gevent import GreenletExit
+from traceback import print_exc
+
+i = 0
+max_age = 0.0
+request_time = 0.0
+in_flight = 0
+failed_requests = 0
+
+
+def run_client_bench(client, concurrency):
+    def do_work():
+        global i, last, failed_requests, max_age, request_time, in_flight
+        start_time = time.time()
+
+        try:
+            in_flight += 1
+            res = client.send_request('hello world')
+            # gevent.sleep(0)
+            # print repr(res)
+            assert res == 'hello world'
+            i += 1
+        except GreenletExit:
+            raise
+
+        except:
+            failed_requests += 1
+            print_exc()
+
+        finally:
+            age = time.time() - start_time
+            request_time += age
+            if age > max_age:
+                max_age = age
+
+            in_flight -= 1
+
+    def log_loop():
+        global max_age, request_time
+        last_i = 0
+        last = time.time()
+        while True:
+            gevent.sleep(1)
+            now = time.time()
+            elapsed = now - last
+            req_sec = (i - last_i) / elapsed
+            avg_time = request_time / (i - last_i)
+            # max_age = max(now - r.start_time for r in in_flight) if in_flight else 0
+            print '%s total requests (%.2f/sec). last log %.2f sec ago. %s failed, %s in flight, %.2f ms max, %.2f ms avg response time' % (
+                i, req_sec, elapsed, failed_requests, in_flight, max_age * 1000, avg_time * 1000
+            )
+            last_i = i
+            last = now
+            max_age = 0
+            request_time = 0
+
+    def work_loop():
+        print 'spawning work loop'
+        global i, last
+
+        while True:
+            do_work()
+
+    import gevent
+    client.send_request('hi')
+    greenlets = [gevent.spawn(log_loop)]
+    for _ in xrange(concurrency):
+        greenlets.append(gevent.spawn(work_loop))
+
+    import signal
+
+    signal.signal(signal.SIGINT, lambda *_: gevent.killall(greenlets, block=False))
+
+    gevent.joinall(greenlets)
+    print 'All greenlets have exited.'
