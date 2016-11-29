da732f71db162c34752b8601373e467a2781c0e2
jhowarth <jahowarth@gmail.com>
2016-11-29 11:47:55 -0800

    code cleanup

--- ex/loqui/lib/types.ex
+++ ex/loqui/lib/types.ex
@@ -1,16 +1,6 @@
 defmodule Loqui.Types do
   defmacro __using__(_) do
     quote do
-      @opcode_hello 1
-      @opcode_hello_ack 2
-      @opcode_ping 3
-      @opcode_pong 4
-      @opcode_request 5
-      @opcode_response 6
-      @opcode_push 7
-      @opcode_goaway 8
-      @opcode_error 9
-
       defmacro uint8 do
         quote do: unsigned-integer-size(8)
       end
