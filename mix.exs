8688b4664944fbc2ffe26b746d4ce2bf32de8a81
jhowarth <jahowarth@gmail.com>
2016-12-01 17:24:55 -0800

    set elixirc_path

--- mix.exs
+++ mix.exs
@@ -8,7 +8,8 @@ defmodule Loqui.Mixfile do
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps
+      deps: deps,
+      elixirc_paths: ["ex/lib/"]
     ]
   end
 
