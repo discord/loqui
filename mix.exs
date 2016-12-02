3e461df31e39706ceb162e73422fb1e85f36cded
Jake <me@jh.gg>
2016-12-02 13:33:40 -0800

    Rev version to 0.2.2 for py/ex

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Loqui.Mixfile do
   def project do
     [
       app: :loqui,
-      version: "0.0.1",
+      version: "0.2.2",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
