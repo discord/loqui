84c1e5e69f7691cb4a5eb399cf0687a3199ab8b8
jhowarth <jahowarth@gmail.com>
2016-12-19 15:08:49 -0800

    prep 0.2.8

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ if cythonize:
 
 setup(
     name='loqui',
-    version='0.2.7',
+    version='0.2.8',
     author='Jake Heinz',
     author_email='jh@discordapp.com',
     url="http://github.com/hammerandchisel/loqui",
