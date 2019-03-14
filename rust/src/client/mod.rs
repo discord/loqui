0b15daeb7c9aff2529de8524131d4bf330c57858
jhowarth <jahowarth@gmail.com>
2019-03-13 17:55:02 -0700

    code cleanup

--- rust/src/client/mod.rs
+++ rust/src/client/mod.rs
@@ -23,35 +23,6 @@ pub struct Client {
     waiters: Arc<RwLock<HashMap<u32, Sender<String>>>>,
 }
 
-struct ResponseFuture {
-    seq: u32,
-    waiters: Arc<RwLock<HashMap<u32, Sender<String>>>>,
-}
-
-/*
-impl StdFuture for ResponseFuture {
-    type Output = Result<String, Error>;
-
-    fn poll(mut self: Pin<&mut Self>, waker: &Waker) -> StdPoll<Self::Output> {
-        println!("poll");
-        match self.waiters.as_ref().try_read() {
-            Ok(waiters) => {
-                match waiters.get(&self.seq) {
-                    // TODO: need to remove it from waiters
-                    Some(Some(response)) => StdPoll::Ready(Ok(response.clone())),
-                    Some(None) => {
-                        println!("pending");
-                        StdPoll::Pending
-                    },
-                    None => StdPoll::Ready(Err(err_msg("Waiting for a sequence that doesn't exist"))),
-                }
-            },
-            Err(e) => StdPoll::Pending,
-        }
-    }
-}
-*/
-
 impl Client {
     pub async fn connect<A: AsRef<str>>(address: A) -> Result<Client, Error> {
         let addr: SocketAddr = address.as_ref().parse()?;
@@ -60,13 +31,13 @@ impl Client {
         let waiters : Arc<RwLock<HashMap<u32, Sender<String>>>> = Arc::new(RwLock::new(HashMap::new()));
         let read_waiters = waiters.clone();
 
+        // read task
         tokio::spawn_async(async move {
             let mut data = [0; 1024];
             while let Ok(_bytes_read) = await!(reader.read_async(&mut data)) {
                 println!("received data from server {:?}", data.to_vec());
                 let sender = read_waiters.write().unwrap().remove(&1).unwrap();
                 sender.send(String::from_utf8(data.to_vec()).unwrap());
-                //read_waiters.write().unwrap().insert(1, Some("done".to_string()));
             }
         });
 
@@ -94,15 +65,8 @@ impl Client {
         let data = message.as_bytes();
         await!(self.write(data))?;
         let seq = self.next_seq();
-        // TODO:
         let (sender, receiver) = oneshot();
         self.waiters.write().unwrap().insert(seq, sender);
-        /*
-        await!(ResponseFuture {
-            waiters: self.waiters.clone(),
-            seq,
-        })
-        */
         let result = await!(receiver)?;
         Ok(result)
     }
