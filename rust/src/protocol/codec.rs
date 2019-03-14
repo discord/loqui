5856030ff18b9e4fb9baca275bcfd57d211c3aad
jhowarth <jahowarth@gmail.com>
2019-03-14 15:35:21 -0700

    add more

---
+++ rust/src/protocol/codec.rs
@@ -0,0 +1,225 @@
+use tokio_codec::{Decoder, Encoder};
+use bytes::BytesMut;
+
+use crate::protocol::frames;
+use crate::protocol::errors::{ProtocolError};
+
+#[derive(Debug, PartialEq)]
+pub enum LoquiFrame {
+    Hello(frames::Hello),
+    HelloAck(frames::HelloAck),
+    Ping(frames::Ping),
+    Pong(frames::Pong),
+    Request(frames::Request),
+    Response(frames::Response),
+    Push(frames::Push),
+    GoAway(frames::GoAway),
+    Error(frames::Error),
+}
+
+
+#[derive(Debug)]
+pub struct LoquiCodec {
+    pub max_payload_bytes: u32,
+}
+
+impl LoquiCodec {
+    pub fn new(max_payload_bytes: u32) -> LoquiCodec {
+        LoquiCodec{
+            max_payload_bytes: max_payload_bytes,
+        }
+    }
+}
+
+impl Encoder for LoquiCodec {
+    type Item = LoquiFrame;
+    type Error = ::failure::Error;
+
+    fn encode(&mut self, item: Self::Item, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        match item {
+            LoquiFrame::Hello(mut frame)    => frame.encode(dst),
+            LoquiFrame::HelloAck(mut frame) => frame.encode(dst),
+            LoquiFrame::Ping(mut frame)     => frame.encode(dst),
+            LoquiFrame::Pong(mut frame)     => frame.encode(dst),
+            LoquiFrame::Request(mut frame)  => frame.encode(dst),
+            LoquiFrame::Response(mut frame) => frame.encode(dst),
+            LoquiFrame::Push(mut frame)     => frame.encode(dst),
+            LoquiFrame::GoAway(mut frame)   => frame.encode(dst),
+            LoquiFrame::Error(mut frame)    => frame.encode(dst),
+        }.map_err(|err| err.into())
+    }
+}
+
+
+impl Decoder for LoquiCodec {
+    type Item = LoquiFrame;
+    type Error = ::failure::Error;
+
+    fn decode(&mut self, buf: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        if buf.is_empty() {
+            return Ok(None);
+        }
+
+        dbg!(buf.clone());
+
+        let op_code = buf[0];
+        Ok(match op_code {
+            frames::Hello::OP_CODE => frames::Hello::decode(self, buf)?
+                .map(|frame| LoquiFrame::Hello(frame)),
+            frames::HelloAck::OP_CODE => frames::HelloAck::decode(self, buf)?
+                .map(|frame| LoquiFrame::HelloAck(frame)),
+            frames::Ping::OP_CODE => frames::Ping::decode(self, buf)?
+                .map(|frame| LoquiFrame::Ping(frame)),
+            frames::Pong::OP_CODE => frames::Pong::decode(self, buf)?
+                .map(|frame| LoquiFrame::Pong(frame)),
+            frames::Request::OP_CODE => frames::Request::decode(self, buf)?
+                .map(|frame| LoquiFrame::Request(frame)),
+            frames::Response::OP_CODE => frames::Response::decode(self, buf)?
+                .map(|frame| LoquiFrame::Response(frame)),
+            frames::Push::OP_CODE => frames::Push::decode(self, buf)?
+                .map(|frame| LoquiFrame::Push(frame)),
+            frames::GoAway::OP_CODE => frames::GoAway::decode(self, buf)?
+                .map(|frame| LoquiFrame::GoAway(frame)),
+            frames::Error::OP_CODE => frames::Error::decode(self, buf)?
+                .map(|frame| LoquiFrame::Error(frame)),
+            // TODO: we need to drop the bad stuff from this byte buffer, otherwise it loops forever
+            _ => return Err(ProtocolError::InvalidOpcode(op_code).into()),
+        })
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use bytes::{BytesMut, BufMut};
+
+    fn test_frame_roundtrip(frame_bytes: &[u8], expected_frame: LoquiFrame) {
+        let mut codec = LoquiCodec::new(500);
+        let buf = &mut BytesMut::with_capacity(1024);
+
+        // Incomplete Payload
+        buf.put(frame_bytes[..frame_bytes.len()-1].to_vec());
+        assert_eq!(codec.decode(buf).unwrap(), None);
+
+        // Complete Frame
+        buf.put(frame_bytes[frame_bytes.len()-1..].to_vec());
+        assert_eq!(codec.decode(buf).unwrap().unwrap(), expected_frame);
+
+        // Buffer has been consumed
+        assert!(buf.is_empty());
+        assert_eq!(codec.decode(buf).unwrap(), None);
+
+        // Encode a frame to make sure it can round-trip.
+        let _ = codec.encode(expected_frame, buf);
+        assert_eq!(buf, frame_bytes);
+    }
+
+    #[test]
+    fn test_hello() {
+        test_frame_roundtrip(
+            &b"\x01\x0f\x01\x00\x00\x00\x16msgpack,json|gzip,lzma"[..],
+            LoquiFrame::Hello(frames::Hello{
+                flags: 15,
+                version: 1,
+                encodings: vec!["msgpack".into(), "json".into()],
+                compressions: vec!["gzip".into(), "lzma".into()],
+            }),
+        );
+    }
+
+    #[test]
+    fn test_helloack() {
+        test_frame_roundtrip(
+            &b"\x02\x0f\x00\x00}\x00\x00\x00\x00\x0cmsgpack|gzip"[..],
+            LoquiFrame::HelloAck(frames::HelloAck{
+                flags: 15,
+                ping_interval_ms: 32000,
+                encoding: "msgpack".into(),
+                compression: "gzip".into(),
+            }),
+        );
+    }
+
+    #[test]
+    fn test_ping() {
+        test_frame_roundtrip(
+            &b"\x03\x0f\x00\x00\x00\x01"[..],
+            LoquiFrame::Ping(frames::Ping{
+                flags: 15,
+                sequence_id: 1,
+            }),
+        );
+    }
+
+    #[test]
+    fn test_pong() {
+        test_frame_roundtrip(
+            &b"\x04\x0f\x00\x00\x00\x01"[..],
+            LoquiFrame::Pong(frames::Pong{
+                flags: 15,
+                sequence_id: 1,
+            }),
+        );
+    }
+
+    #[test]
+    fn test_request() {
+        test_frame_roundtrip(
+            &b"\x05\x1f\x00\x00\x00\x01\x00\x00\x00\x15hello this is my data"[..],
+            LoquiFrame::Request(frames::Request {
+                flags: 31,
+                sequence_id: 1,
+                payload: b"hello this is my data".to_vec(),
+            }),
+        );
+    }
+
+    #[test]
+    fn test_response() {
+        test_frame_roundtrip(
+            &b"\x06\x1f\x00\x00\x0b\xb8\x00\x00\x00\x15hello this is my data"[..],
+            LoquiFrame::Response(frames::Response {
+                flags: 31,
+                sequence_id: 3000,
+                payload: b"hello this is my data".to_vec(),
+            }),
+        );
+    }
+
+    #[test]
+    fn test_push() {
+        test_frame_roundtrip(
+            &b"\x07[\x00\x00\x00\x15hello this is my push"[..],
+            LoquiFrame::Push(frames::Push {
+                flags: 91,
+                payload: b"hello this is my push".to_vec(),
+            }),
+        );
+    }
+
+    #[test]
+    fn test_goaway() {
+        test_frame_roundtrip(
+            &b"\x08\x97#)\x00\x00\x00\x0bgo away pls"[..],
+            LoquiFrame::GoAway(frames::GoAway {
+                flags: 151,
+                code: 9001,
+                payload: b"go away pls".to_vec(),
+            }),
+        );
+    }
+
+    #[test]
+    fn test_error() {
+        test_frame_roundtrip(
+            &b"\t\x97\x00\r\xbc\x04\x05\xa4\x00\x00\x00\x08errrror!"[..],
+            LoquiFrame::Error(frames::Error {
+                flags: 151,
+                code: 1444,
+                sequence_id: 900100,
+                payload: b"errrror!".to_vec(),
+            }),
+        );
+    }
+}
---
+++ rust/src/protocol/frames.rs
@@ -0,0 +1,561 @@
+use bytes::{BufMut, BytesMut};
+use byteorder::{BigEndian, ByteOrder};
+
+use crate::protocol::codec::{LoquiCodec};
+use crate::protocol::errors::{ProtocolError};
+
+type DecodeResult<T> = Result<Option<T>, ProtocolError>;
+type EncodeResult = Result<(), ProtocolError>;
+
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Hello {
+    pub flags: u8,
+    pub version: u8,
+    pub encodings: Vec<String>,
+    pub compressions: Vec<String>,
+}
+
+
+impl Hello {
+    pub const OP_CODE: u8 = 1;
+    pub const MIN_BYTES: u8 = 7;
+
+    pub fn encode(&mut self, dst: &mut BytesMut) -> EncodeResult {
+        dst.put_u8(Self::OP_CODE);
+        dst.put_u8(self.flags);
+        dst.put_u8(self.version);
+
+        let payload = format!(
+            "{}|{}",
+            self.encodings.join(","),
+            self.compressions.join(","),
+        );
+        let payload = payload.bytes().collect::<Vec<u8>>();
+
+        dst.put_u32_be(payload.len() as u32);
+        dst.put(payload);
+        Ok(())
+    }
+
+    pub fn decode(codec: &mut LoquiCodec, buf: &mut BytesMut) -> DecodeResult<Self> {
+        if buf.len() < Self::MIN_BYTES as usize {
+            // Not enough data for a frame. Wait for more.
+            return Ok(None);
+        }
+
+        let flags = buf[1];
+        let version = buf[2];
+        let payload_bytes = BigEndian::read_u32(&buf[3..7]);
+
+        if payload_bytes > codec.max_payload_bytes {
+            return Err(ProtocolError::PayloadTooLarge(payload_bytes, codec.max_payload_bytes));
+        }
+
+        let required_len = Self::MIN_BYTES as usize + payload_bytes as usize;
+        if buf.len() < required_len {
+            // Don't have complete payload. Wait for more.
+            return Ok(None);
+        }
+
+        // We have enough information to determine whether this is a valid
+        // frame or not so split out the frame.
+        let frame = buf.split_to(required_len);
+        let payload = ::std::str::from_utf8(&frame[7..required_len])
+            .map_err(|_| ProtocolError::InvalidPayload("Failed to decode as string".into()))?;
+
+        let settings: Vec<&str> = payload.split("|").collect();
+        if settings.len() != 2 {
+            return Err(ProtocolError::InvalidPayload("Expected exactly two settings.".into()));
+        }
+
+        let encodings = settings[0]
+            .split_terminator(",")
+            .map(|s| String::from(s))
+            .collect::<Vec<String>>();
+
+        let compressions = settings[1]
+            .split_terminator(",")
+            .map(|s| String::from(s))
+            .collect::<Vec<String>>();
+
+        Ok(Some(Self {
+            flags: flags,
+            version: version,
+            encodings: encodings,
+            compressions: compressions,
+        }))
+    }
+
+
+}
+
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct HelloAck {
+    pub flags: u8,
+    pub ping_interval_ms: u32,
+    pub encoding: String,
+    pub compression: String,
+}
+
+
+impl HelloAck {
+    pub const OP_CODE: u8 = 2;
+    pub const MIN_BYTES: u8 = 10;
+
+    pub fn encode(&mut self, dst: &mut BytesMut) -> EncodeResult {
+        dst.put_u8(Self::OP_CODE);
+        dst.put_u8(self.flags);
+        dst.put_u32_be(self.ping_interval_ms);
+
+        let payload = format!("{}|{}", self.encoding, self.compression);
+        dst.put_u32_be(payload.len() as u32);
+        dst.put(payload);
+        Ok(())
+    }
+
+    pub fn decode(codec: &mut LoquiCodec, buf: &mut BytesMut) -> DecodeResult<Self> {
+        if buf.len() < Self::MIN_BYTES as usize {
+            // Not enough data for a frame. Wait for more.
+            return Ok(None);
+        }
+
+        let flags = buf[1];
+        let ping_interval_ms = BigEndian::read_u32(&buf[2..6]);
+        let payload_bytes = BigEndian::read_u32(&buf[6..10]);
+
+        if payload_bytes > codec.max_payload_bytes {
+            return Err(ProtocolError::PayloadTooLarge(payload_bytes, codec.max_payload_bytes));
+        }
+
+        let required_len = Self::MIN_BYTES as usize + payload_bytes as usize;
+        if buf.len() < required_len {
+            // Don't have complete payload. Wait for more.
+            return Ok(None);
+        }
+
+        // We have enough information to determine whether this is a valid
+        // frame or not so split out the frame.
+        let frame = buf.split_to(required_len);
+        let payload = ::std::str::from_utf8(&frame[10..required_len])
+            .map_err(|_| ProtocolError::InvalidPayload("Failed to decode as string".into()))?;
+
+        let settings: Vec<&str> = payload.split("|").collect();
+        if settings.len() != 2 {
+            return Err(ProtocolError::InvalidPayload("Expected exactly two settings.".into()));
+        }
+
+        Ok(Some(Self {
+            flags: flags,
+            ping_interval_ms: ping_interval_ms,
+            encoding: settings[0].into(),
+            compression: settings[1].into(),
+        }))
+    }
+
+    fn negotiate_encoding(server_encodings: &[String], client_encodings: &[String])
+        -> Option<String>
+    {
+        for server_encoding in server_encodings {
+            for client_encoding in client_encodings {
+                if server_encoding == client_encoding {
+                    return Some(server_encoding.clone())
+                }
+            }
+        }
+        None
+    }
+
+    fn negotiate_compression(server_compressions: &[String], client_compressions: &[String])
+        -> Option<String>
+    {
+        for server_compression in server_compressions {
+            for client_compression in client_compressions {
+                if server_compression == client_compression {
+                    return Some(server_compression.clone())
+                }
+            }
+        }
+        None
+    }
+
+    // Negotiates a HelloAck frame from a &Hello.
+    pub fn from_hello(
+        hello: &Hello,
+        ping_interval_ms: u32,
+        encodings: &[String],
+        compressions: &[String],
+    ) -> Option<Self> {
+
+        let encoding = HelloAck::negotiate_encoding(encodings, hello.encodings.as_ref())?;
+
+        let compression = HelloAck::negotiate_compression(
+            compressions, hello.compressions.as_ref()
+        ).unwrap_or(String::from(""));
+
+        Some(HelloAck {
+            flags: 0,
+            ping_interval_ms: ping_interval_ms,
+            encoding: encoding,
+            compression: compression,
+        })
+    }
+}
+
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Ping {
+    pub flags: u8,
+    pub sequence_id: u32,
+}
+
+impl Ping {
+    pub const OP_CODE: u8 = 3;
+    pub const MIN_BYTES: u8 = 6;
+
+    pub fn encode(&mut self, dst: &mut BytesMut) -> EncodeResult {
+        //dst.resize((8 + 8 + 32), 0);
+        let remaining = dst.remaining_mut();
+        let missing = remaining - 8 + 8 + 32;
+        if missing < 0 {
+            dst.reserve(0 - missing);
+        }
+
+        dst.put_u8(Self::OP_CODE);
+        dst.put_u8(self.flags);
+        dst.put_u32_be(self.sequence_id);
+        Ok(())
+    }
+
+    pub fn decode(_codec: &mut LoquiCodec, buf: &mut BytesMut) -> DecodeResult<Self> {
+        if buf.len() < Self::MIN_BYTES as usize {
+            // Not enough data for a frame. Wait for more.
+            return Ok(None);
+        }
+
+        let flags = buf[1];
+        let sequence_id = BigEndian::read_u32(&buf[2..6]);
+
+        // We have enough information to determine whether this is a valid
+        // frame or not so advance the buffer.
+        buf.advance(Self::MIN_BYTES as usize);
+
+        Ok(Some(Self {
+            flags: flags,
+            sequence_id: sequence_id,
+        }))
+    }
+}
+
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Pong {
+    pub flags: u8,
+    pub sequence_id: u32,
+}
+
+impl Pong {
+    pub const OP_CODE: u8 = 4;
+    pub const MIN_BYTES: u8 = 6;
+
+    pub fn encode(&mut self, dst: &mut BytesMut) -> EncodeResult {
+        dst.put_u8(Self::OP_CODE);
+        dst.put_u8(self.flags);
+        dst.put_u32_be(self.sequence_id);
+        Ok(())
+    }
+
+    pub fn decode(_codec: &mut LoquiCodec, buf: &mut BytesMut) -> DecodeResult<Self> {
+        if buf.len() < Self::MIN_BYTES as usize {
+            // Not enough data for a frame. Wait for more.
+            return Ok(None);
+        }
+
+        let flags = buf[1];
+        let sequence_id = BigEndian::read_u32(&buf[2..6]);
+
+        // We have enough information to determine whether this is a valid
+        // frame or not so advance the buffer.
+        buf.advance(Self::MIN_BYTES as usize);
+
+        Ok(Some(Self {
+            flags: flags,
+            sequence_id: sequence_id,
+        }))
+    }
+}
+
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Request {
+    pub flags: u8,
+    pub sequence_id: u32,
+    pub payload: Vec<u8>,
+}
+
+impl Request {
+    pub const OP_CODE: u8 = 5;
+    pub const MIN_BYTES: u8 = 10;
+
+    pub fn encode(&mut self, dst: &mut BytesMut) -> EncodeResult {
+        dst.put_u8(Self::OP_CODE);
+        dst.put_u8(self.flags);
+        dst.put_u32_be(self.sequence_id);
+        dst.put_u32_be(self.payload.len() as u32);
+        dst.put(self.payload.clone());
+        Ok(())
+    }
+
+    pub fn decode(codec: &mut LoquiCodec, buf: &mut BytesMut) -> DecodeResult<Self> {
+        if buf.len() < Self::MIN_BYTES as usize {
+            // Not enough data for a frame. Wait for more.
+            return Ok(None);
+        }
+
+        let flags = buf[1];
+        let sequence_id = BigEndian::read_u32(&buf[2..6]);
+        let payload_bytes = BigEndian::read_u32(&buf[6..10]);
+
+        if payload_bytes > codec.max_payload_bytes {
+            return Err(ProtocolError::PayloadTooLarge(payload_bytes, codec.max_payload_bytes));
+        }
+
+        let required_len = Self::MIN_BYTES as usize + payload_bytes as usize;
+        if buf.len() < required_len {
+            // Don't have complete payload. Wait for more.
+            return Ok(None);
+        }
+
+        // We have enough information to determine whether this is a valid
+        // frame or not so split out the frame.
+        let frame = buf.split_to(required_len);
+        let payload = frame[10..required_len].to_vec();
+
+        Ok(Some(Self {
+            flags: flags,
+            sequence_id: sequence_id,
+            payload: payload,
+        }))
+    }
+}
+
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Response {
+    pub flags: u8,
+    pub sequence_id: u32,
+    pub payload: Vec<u8>,
+}
+
+
+impl Response {
+    pub const OP_CODE: u8 = 6;
+    pub const MIN_BYTES: u8 = 10;
+
+    pub fn encode(&mut self, dst: &mut BytesMut) -> EncodeResult {
+        dst.put_u8(Self::OP_CODE);
+        dst.put_u8(self.flags);
+        dst.put_u32_be(self.sequence_id);
+        dst.put_u32_be(self.payload.len() as u32);
+        dst.put(self.payload.clone());
+        Ok(())
+    }
+
+    pub fn decode(codec: &mut LoquiCodec, buf: &mut BytesMut) -> DecodeResult<Self> {
+        if buf.len() < Self::MIN_BYTES as usize {
+            // Not enough data for a frame. Wait for more.
+            return Ok(None);
+        }
+
+        let flags = buf[1];
+        let sequence_id = BigEndian::read_u32(&buf[2..6]);
+        let payload_bytes = BigEndian::read_u32(&buf[6..10]);
+
+        if payload_bytes > codec.max_payload_bytes {
+            return Err(ProtocolError::PayloadTooLarge(payload_bytes, codec.max_payload_bytes));
+        }
+
+        let required_len = Self::MIN_BYTES as usize + payload_bytes as usize;
+        if buf.len() < required_len {
+            // Don't have complete payload. Wait for more.
+            return Ok(None);
+        }
+
+        // We have enough information to determine whether this is a valid
+        // frame or not so split out the frame.
+        let frame = buf.split_to(required_len);
+        let payload = frame[10..required_len].to_vec();
+
+        Ok(Some(Self {
+            flags: flags,
+            sequence_id: sequence_id,
+            payload: payload,
+        }))
+    }
+}
+
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Push {
+    pub flags: u8,
+    pub payload: Vec<u8>,
+}
+
+
+impl Push {
+    pub const OP_CODE: u8 = 7;
+    pub const MIN_BYTES: u8 = 6;
+
+    pub fn encode(&mut self, dst: &mut BytesMut) -> EncodeResult {
+        dst.put_u8(Self::OP_CODE);
+        dst.put_u8(self.flags);
+        dst.put_u32_be(self.payload.len() as u32);
+        dst.put(self.payload.clone());
+        Ok(())
+    }
+
+    pub fn decode(codec: &mut LoquiCodec, buf: &mut BytesMut) -> DecodeResult<Self> {
+        if buf.len() < Self::MIN_BYTES as usize {
+            // Not enough data for a frame. Wait for more.
+            return Ok(None);
+        }
+
+        let flags = buf[1];
+        let payload_bytes = BigEndian::read_u32(&buf[2..6]);
+
+        if payload_bytes > codec.max_payload_bytes {
+            return Err(ProtocolError::PayloadTooLarge(payload_bytes, codec.max_payload_bytes));
+        }
+
+        let required_len = Self::MIN_BYTES as usize + payload_bytes as usize;
+        if buf.len() < required_len {
+            // Don't have complete payload. Wait for more.
+            return Ok(None);
+        }
+
+        // We have enough information to determine whether this is a valid
+        // frame or not so split out the frame.
+        let frame = buf.split_to(required_len);
+        let payload = frame[6..required_len].to_vec();
+
+        Ok(Some(Self {
+            flags: flags,
+            payload: payload,
+        }))
+    }
+}
+
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct GoAway {
+    pub flags: u8,
+    pub code: u16,
+    pub payload: Vec<u8>,
+}
+
+
+impl GoAway {
+    pub const OP_CODE: u8 = 8;
+    pub const MIN_BYTES: u8 = 8;
+
+    pub fn encode(&mut self, dst: &mut BytesMut) -> EncodeResult {
+        dst.put_u8(Self::OP_CODE);
+        dst.put_u8(self.flags);
+        dst.put_u16_be(self.code);
+        dst.put_u32_be(self.payload.len() as u32);
+        dst.put(self.payload.clone());
+        Ok(())
+    }
+
+    pub fn decode(codec: &mut LoquiCodec, buf: &mut BytesMut) -> DecodeResult<Self> {
+        if buf.len() < Self::MIN_BYTES as usize {
+            // Not enough data for a frame. Wait for more.
+            return Ok(None);
+        }
+
+        let flags = buf[1];
+        let code = BigEndian::read_u16(&buf[2..4]);
+        let payload_bytes = BigEndian::read_u32(&buf[4..8]);
+
+        if payload_bytes > codec.max_payload_bytes {
+            return Err(ProtocolError::PayloadTooLarge(payload_bytes, codec.max_payload_bytes));
+        }
+
+        let required_len = Self::MIN_BYTES as usize + payload_bytes as usize;
+        if buf.len() < required_len {
+            // Don't have complete payload. Wait for more.
+            return Ok(None);
+        }
+
+        // We have enough information to determine whether this is a valid
+        // frame or not so split out the frame.
+        let frame = buf.split_to(required_len);
+        let payload = frame[8..required_len].to_vec();
+
+        Ok(Some(Self {
+            flags: flags,
+            code: code,
+            payload: payload,
+        }))
+    }
+}
+
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Error {
+    pub flags: u8,
+    pub sequence_id: u32,
+    pub code: u16,
+    pub payload: Vec<u8>,
+}
+
+
+impl Error {
+    pub const OP_CODE: u8 = 9;
+    pub const MIN_BYTES: u8 = 12;
+
+    pub fn encode(&mut self, dst: &mut BytesMut) -> EncodeResult {
+        dst.put_u8(Self::OP_CODE);
+        dst.put_u8(self.flags);
+        dst.put_u32_be(self.sequence_id);
+        dst.put_u16_be(self.code);
+        dst.put_u32_be(self.payload.len() as u32);
+        dst.put(self.payload.clone());
+        Ok(())
+    }
+
+    pub fn decode(codec: &mut LoquiCodec, buf: &mut BytesMut) -> DecodeResult<Self> {
+        if buf.len() < Self::MIN_BYTES as usize {
+            // Not enough data for a frame. Wait for more.
+            return Ok(None);
+        }
+
+        let flags = buf[1];
+        let sequence_id = BigEndian::read_u32(&buf[2..6]);
+        let code = BigEndian::read_u16(&buf[6..8]);
+        let payload_bytes = BigEndian::read_u32(&buf[8..12]);
+
+        if payload_bytes > codec.max_payload_bytes {
+            return Err(ProtocolError::PayloadTooLarge(payload_bytes, codec.max_payload_bytes));
+        }
+
+        let required_len = Self::MIN_BYTES as usize + payload_bytes as usize;
+        if buf.len() < required_len {
+            // Don't have complete payload. Wait for more.
+            return Ok(None);
+        }
+
+        // We have enough information to determine whether this is a valid
+        // frame or not so split out the frame.
+        let frame = buf.split_to(required_len);
+        let payload = frame[12..required_len].to_vec();
+
+        Ok(Some(Self {
+            flags: flags,
+            sequence_id: sequence_id,
+            code: code,
+            payload: payload,
+        }))
+    }
+}
