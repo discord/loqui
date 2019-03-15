ed204029de3090d96195f4bee472427e6db2a735
jhowarth <jahowarth@gmail.com>
2019-03-14 18:09:55 -0700

    code cleanup

--- rust/src/client/mod.rs
+++ rust/src/client/mod.rs
@@ -42,6 +42,61 @@ enum Message {
     Response(LoquiFrame),
 }
 
+struct MessageHandler {
+    waiters: HashMap<u32, OneShotSender<Result<Vec<u8>, Error>>>,
+    next_seq: u32,
+}
+
+impl MessageHandler {
+    fn new() -> Self {
+        Self {
+            // TODO: should probably sweep these, probably request timeout
+            waiters: HashMap::new(),
+            next_seq: 1,
+        }
+    }
+
+    fn next_seq(&mut self) -> u32 {
+        let seq = self.next_seq;
+        self.next_seq += 1;
+        seq
+    }
+
+    fn handle(&mut self, message: Message) -> Option<LoquiFrame> {
+        match message {
+            Message::Request { payload, sender } => {
+                let sequence_id = self.next_seq();
+                self.waiters.insert(sequence_id, sender);
+                Some(LoquiFrame::Request(Request {
+                    sequence_id,
+                    flags: 2,
+                    payload,
+                }))
+            }
+            Message::Push { payload } => {
+                println!("push {:?}", payload);
+                None
+            }
+            Message::Response(frame) => {
+                match frame {
+                    LoquiFrame::Response(Response {
+                        flags,
+                        sequence_id,
+                        payload,
+                    }) => {
+                        let sender = self.waiters.remove(&sequence_id).unwrap();
+                        sender.send(Ok(payload));
+                    }
+                    frame => {
+                        dbg!(frame);
+                    }
+                }
+                None
+            }
+        }
+    }
+}
+
 struct SocketHandler {}
 
 impl SocketHandler {
@@ -51,42 +106,19 @@ impl SocketHandler {
 
         let framed_socket = Framed::new(socket, LoquiCodec::new(50000));
         let (mut writer, mut reader) = framed_socket.split();
-        // TODO: should probably sweep these, probably request timeout
-        let mut waiters: HashMap<u32, OneShotSender<Result<Vec<u8>, Error>>> = HashMap::new();
-        let mut next_seq = 1;
+        let mut message_handler = MessageHandler::new();
+
         let mut stream = reader
             .map(|frame| Message::Response(frame))
             .select(rx.map_err(|()| err_msg("rx error")));
 
-        while let Some(item) = await!(stream.next()) {
-            match item {
-                Ok(Message::Request { payload, sender }) => {
-                    let seq = next_seq;
-                    next_seq += 1;
-                    waiters.insert(seq, sender);
-                    writer = await!(writer.send(LoquiFrame::Request(Request {
-                        sequence_id: seq,
-                        flags: 2,
-                        payload,
-                    })))
-                    .unwrap();
-                }
-                Ok(Message::Push { payload }) => {
-                    println!("push {:?}", payload);
-                }
-                Ok(Message::Response(frame)) => match frame {
-                    LoquiFrame::Response(Response {
-                        flags,
-                        sequence_id,
-                        payload,
-                    }) => {
-                        let sender = waiters.remove(&sequence_id).unwrap();
-                        sender.send(Ok(payload));
+        while let Some(message) = await!(stream.next()) {
+            match message {
+                Ok(message) => {
+                    if let Some(frame) = message_handler.handle(message) {
+                        writer = await!(writer.send(frame)).unwrap();
                     }
-                    frame => {
-                        dbg!(frame);
-                    }
-                },
+                }
                 Err(e) => {
                     dbg!(e);
                 }
