d283e343aa1c08c1187e9e97af3c15b71689801e
jhowarth <jahowarth@gmail.com>
2019-05-21 10:10:25 -0700

    add link

--- rust/loqui_client/src/lib.rs
+++ rust/loqui_client/src/lib.rs
@@ -6,6 +6,7 @@ extern crate log;
 mod client;
 mod config;
 // XXX: This is required to be public due to a bug in the rust compiler.
+// https://github.com/rust-lang/rust/issues/50865
 pub mod connection_handler;
 #[cfg(test)]
 mod future_utils;
